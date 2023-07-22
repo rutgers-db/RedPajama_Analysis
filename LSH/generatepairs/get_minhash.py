166587a8b52d2e2327b553087134825e658c3039
pzcddm <786858269@qq.com>
2023-07-22 06:41:09 -0400

    tweak

--- LSH/generatepairs/get_minhash.py
+++ LSH/generatepairs/get_minhash.py
@@ -19,6 +19,7 @@ def output_results(output_dir, results, chunk_id, iter):
         pickle.dump(results, fout)
 
 def generate_minhash(args):
+# def generate_minhash(dataset_name,chunk_id, num_perm):
     dataset_name,chunk_id, num_perm = args
     gc.collect()
     parent_dir = "/common/users/zp128/RedPajama_Tokenized/" + dataset_name
@@ -46,8 +47,8 @@ def generate_minhash(args):
         )
 
     # write the buckets into the file
-    return buckets
-    # output_results(parent_dir, buckets, chunk_id, 0)
+    # return buckets
+    output_results(parent_dir, buckets, chunk_id, 0)
 
 
 if __name__ == "__main__":
@@ -103,11 +104,12 @@ if __name__ == "__main__":
         output_results(parent_dir, results, chunk_id, 0)
 
     #Producer
-    # for chunk_id in range(chunk_amount): #(2,35,41): #
+    # for chunk_id in (1, 2, 4, 9, 12, 32, 34, 35, 36, 41, 51, 52, 53, 54, 55, 56, 58, 59, 60, 61, 65, 66, 67, 69, 70): #(2,35,41): #
     #     # Producer
+    #     tmp_args = [dataset_name, chunk_id, num_perm]
     #     p = Process(
     #             target=generate_minhash,
-    #             args=(dataset_name, chunk_id),
+    #             args=(dataset_name, chunk_id, num_perm ),
     #         )
     #     processes.append(p)
     #     p.start()
